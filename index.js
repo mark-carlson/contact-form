7d43701e9d5f9a9e85ab4adcb3845aac3f989703
Mark Carlson <mark@markcarlson.io>
2018-12-04 21:18:08 -0800

    Removes index.js from root folder

--- index.js
+++
@@ -1,8 +0,0 @@
-import app from './app';
-import env from './config/env';
-
-const server = app.listen(process.env.PORT || env.port, () => {
-  console.log(`${env.name} server is listening at port ${process.env.PORT || env.port}`);
-});
-
-export default server;
